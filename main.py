0cbd5b15a5dc7459624beda2aac8e1865cd95a07
Sergey <s.gz@yandex.ru>
2022-03-30 15:09:23 +0300

    test 2

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask"
+    return "Привет от приложения Flask \n Новая версия ответа."
+
 
 
 if __name__ == '__main__':
